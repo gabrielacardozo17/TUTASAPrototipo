664051c715422d2014a06125ec640f875325f434
Manuel Ponce de Leon <125145097+notponci@users.noreply.github.com>
2025-10-09 16:52:26 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+namespace TUTASAPrototipo
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            //ApplicationConfiguration.Initialize();
+            //Application.Run(new EmitirFactura.EmitirFacturaForm());
+            //Application.Run(new ConsultarEstado.ConsultarEstadoForm()); 
+            //Application.Run(new ImponerEncomiendaCallCenter.ImponerEncomiendaCallCenterForm());
+            //Application.Run(new ImponerEncomiendaAgencia.ImponerEncomiendaAgenciaForm());
+            //Application.Run(new ImponerEncomiendaCD.ImponerEncomiendaCentroDistribucionForm());
+            //Application.Run(new EntregarEncomiendaEnAgencia.EntregarEncomiendaEnAgenciaForm());
+            //Application.Run(new EntregarEncomiendaCD.EntregarEncomiendaCDForm());   
+            //Application.Run(new MonitoreoResultados.MonitoreoResultadosForm());
+            //Application.Run(new RecepcionAgencia.RecepcionAgenciaForm1());
+            //Application.Run(new RecepcionYDespachoLargaDistancia.RecepcionYDespachoLargaDistanciaForm());
+            //Application.Run(new RecepcionYDespachoUltimaMillaCD.RecepcionYDespachoUltimaMillaForm());
+            //Application.Run(new EstadoCuentaCorrienteCliente.EstadoCuentaCorrienteClienteForm());
+
+        }
+    }
+}
