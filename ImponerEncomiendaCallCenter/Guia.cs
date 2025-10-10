7a332e0d8c6f322d6272455d4f8bcf978c4721c0
Manuel Ponce de León <mponcedeleon02@outlook.com>
2025-10-10 10:19:31 -0300

    .

---
+++ ImponerEncomiendaCallCenter/Guia.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace TUTASAPrototipo.ImponerEncomiendaCallCenter
+{
+    public class Guia
+    {
+        public string NumeroGuia { get; set; }
+        public Cliente Remitente { get; set; }
+        public Destinatario Destinatario { get; set; }
+        public Dictionary<Tamanio, int> Encomiendas { get; set; }
+        public string Estado { get; set; }
+    }
+}
