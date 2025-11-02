0d71ab3c3fd65309b3245fa535c61ddc7411c579
ValentinMancuso <valen.mancuso.m@gmail.com>
2025-11-02 14:32:41 -0300

    Agregar Almacenes que faltaban

---
+++ Almacenes/ConvenioAgenciaAlmacen.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TUTASAPrototipo.Almacenes
+{
+    static class ConvenioAgenciaAlmacen
+    {
+        public static List<ConvenioAgenciaEntidad> convenioAgencias = new List<ConvenioAgenciaEntidad>();
+
+        static ConvenioAgenciaAlmacen()
+        {
+            if (File.Exists("ConvenioAgencias.json"))
+            {
+                var convenioAgenciaJson = File.ReadAllText("ConvenioAgencias.json");
+                convenioAgencias = System.Text.Json.JsonSerializer.Deserialize<List<ConvenioAgenciaEntidad>>(convenioAgenciaJson) ?? new List<ConvenioAgenciaEntidad>();
+            }
+        }
+
+        public static void Grabar()
+        {
+            var convenioAgenciaJson = System.Text.Json.JsonSerializer.Serialize(convenioAgencias);
+            File.WriteAllText("ConvenioAgencias.json", convenioAgenciaJson);
+        }
+    }
+
+}
---
+++ Almacenes/LocalidadAlmacen.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TUTASAPrototipo.Almacenes
+{
+    static class LocalidadAlmacen
+    {
+        public static List<LocalidadEntidad> localidades = new List<LocalidadEntidad>();
+
+        static LocalidadAlmacen()
+        {
+            if (File.Exists("Localidades.json"))
+            {
+                var localidadJson = File.ReadAllText("Localidades.json");
+                localidades = System.Text.Json.JsonSerializer.Deserialize<List<LocalidadEntidad>>(localidadJson) ?? new List<LocalidadEntidad>();
+            }
+        }
+
+        public static void Grabar()
+        {
+            var localidadJson = System.Text.Json.JsonSerializer.Serialize(localidades);
+            File.WriteAllText("Localidades.json", localidadJson);
+        }
+    }
+    
+}
