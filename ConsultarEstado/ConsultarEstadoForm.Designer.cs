6e13086e5246b8237b5ef10d2f9833ecee408dbc
kiararey123 <kiararey.grange@hotmail.com>
2025-11-03 23:02:43 -0300

    Cambios en pantalla Consultar estado

--- ConsultarEstado/ConsultarEstadoForm.Designer.cs
+++ ConsultarEstado/ConsultarEstadoForm.Designer.cs
@@ -49,9 +49,9 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(293, 49);
+            label1.Location = new Point(302, 67);
             label1.Name = "label1";
-            label1.Size = new Size(0, 15);
+            label1.Size = new Size(0, 20);
             label1.TabIndex = 0;
             // 
             // groupBox1
@@ -60,18 +60,21 @@
             groupBox1.Controls.Add(NumGuiaLabel);
             groupBox1.Controls.Add(NroGuiaLabel);
             groupBox1.Controls.Add(NroGuiaBusquedaGroupBox);
-            groupBox1.Location = new Point(61, 36);
+            groupBox1.Location = new Point(58, 51);
+            groupBox1.Margin = new Padding(3, 4, 3, 4);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(434, 125);
+            groupBox1.Padding = new Padding(3, 4, 3, 4);
+            groupBox1.Size = new Size(717, 167);
             groupBox1.TabIndex = 1;
             groupBox1.TabStop = false;
             groupBox1.Text = "Búsqueda de encomienda";
             // 
             // BuscarEstadoGuiaButton
             // 
-            BuscarEstadoGuiaButton.Location = new Point(353, 59);
+            BuscarEstadoGuiaButton.Location = new Point(421, 98);
+            BuscarEstadoGuiaButton.Margin = new Padding(3, 4, 3, 4);
             BuscarEstadoGuiaButton.Name = "BuscarEstadoGuiaButton";
-            BuscarEstadoGuiaButton.Size = new Size(75, 23);
+            BuscarEstadoGuiaButton.Size = new Size(86, 31);
             BuscarEstadoGuiaButton.TabIndex = 6;
             BuscarEstadoGuiaButton.Text = "Buscar";
             BuscarEstadoGuiaButton.UseVisualStyleBackColor = true;
@@ -79,26 +82,28 @@
             // NumGuiaLabel
             // 
             NumGuiaLabel.AutoSize = true;
-            NumGuiaLabel.Location = new Point(6, 62);
+            NumGuiaLabel.Location = new Point(9, 101);
             NumGuiaLabel.Name = "NumGuiaLabel";
-            NumGuiaLabel.Size = new Size(96, 15);
+            NumGuiaLabel.Size = new Size(120, 20);
             NumGuiaLabel.TabIndex = 5;
             NumGuiaLabel.Text = "Número de guía:";
             // 
             // NroGuiaLabel
             // 
             NroGuiaLabel.AutoSize = true;
-            NroGuiaLabel.Location = new Point(6, 28);
+            NroGuiaLabel.Location = new Point(9, 55);
             NroGuiaLabel.Name = "NroGuiaLabel";
-            NroGuiaLabel.Size = new Size(349, 15);
+            NroGuiaLabel.Size = new Size(441, 20);
             NroGuiaLabel.TabIndex = 4;
             NroGuiaLabel.Text = "Ingrese el número de guía de la encomienda que desea consultar";
+            NroGuiaLabel.Click += NroGuiaLabel_Click;
             // 
             // NroGuiaBusquedaGroupBox
             // 
-            NroGuiaBusquedaGroupBox.Location = new Point(108, 59);
+            NroGuiaBusquedaGroupBox.Location = new Point(135, 98);
+            NroGuiaBusquedaGroupBox.Margin = new Padding(3, 4, 3, 4);
             NroGuiaBusquedaGroupBox.Name = "NroGuiaBusquedaGroupBox";
-            NroGuiaBusquedaGroupBox.Size = new Size(233, 23);
+            NroGuiaBusquedaGroupBox.Size = new Size(266, 27);
             NroGuiaBusquedaGroupBox.TabIndex = 3;
             // 
             // ResultadoGroupBox
@@ -106,9 +111,11 @@
             ResultadoGroupBox.Controls.Add(label6);
             ResultadoGroupBox.Controls.Add(label3);
             ResultadoGroupBox.Controls.Add(HistorialGuiaListView);
-            ResultadoGroupBox.Location = new Point(69, 188);
+            ResultadoGroupBox.Location = new Point(58, 250);
+            ResultadoGroupBox.Margin = new Padding(3, 4, 3, 4);
             ResultadoGroupBox.Name = "ResultadoGroupBox";
-            ResultadoGroupBox.Size = new Size(426, 170);
+            ResultadoGroupBox.Padding = new Padding(3, 4, 3, 4);
+            ResultadoGroupBox.Size = new Size(717, 227);
             ResultadoGroupBox.TabIndex = 2;
             ResultadoGroupBox.TabStop = false;
             ResultadoGroupBox.Text = "Resultado de la búsqueda";
@@ -117,26 +124,27 @@
             // 
             label6.AutoSize = true;
             label6.BackColor = SystemColors.ButtonHighlight;
-            label6.Location = new Point(246, 60);
+            label6.Location = new Point(281, 80);
             label6.Name = "label6";
-            label6.Size = new Size(0, 15);
+            label6.Size = new Size(0, 20);
             label6.TabIndex = 5;
             // 
             // label3
             // 
             label3.AutoSize = true;
             label3.BackColor = SystemColors.ButtonHighlight;
-            label3.Location = new Point(29, 60);
+            label3.Location = new Point(33, 80);
             label3.Name = "label3";
-            label3.Size = new Size(0, 15);
+            label3.Size = new Size(0, 20);
             label3.TabIndex = 3;
             // 
             // HistorialGuiaListView
             // 
             HistorialGuiaListView.Columns.AddRange(new ColumnHeader[] { FechaColumn, EstadoColumn, UbicacionColumn });
-            HistorialGuiaListView.Location = new Point(19, 37);
+            HistorialGuiaListView.Location = new Point(22, 49);
+            HistorialGuiaListView.Margin = new Padding(3, 4, 3, 4);
             HistorialGuiaListView.Name = "HistorialGuiaListView";
-            HistorialGuiaListView.Size = new Size(387, 97);
+            HistorialGuiaListView.Size = new Size(670, 128);
             HistorialGuiaListView.TabIndex = 0;
             HistorialGuiaListView.UseCompatibleStateImageBehavior = false;
             HistorialGuiaListView.View = View.Details;
@@ -144,36 +152,38 @@
             // FechaColumn
             // 
             FechaColumn.Text = "Fecha";
-            FechaColumn.Width = 100;
+            FechaColumn.Width = 130;
             // 
             // EstadoColumn
             // 
             EstadoColumn.Text = "Estado";
-            EstadoColumn.Width = 120;
+            EstadoColumn.Width = 280;
             // 
             // UbicacionColumn
             // 
             UbicacionColumn.Text = "Ubicación";
-            UbicacionColumn.Width = 160;
+            UbicacionColumn.Width = 200;
             // 
             // CancelarButton
             // 
-            CancelarButton.Location = new Point(464, 391);
+            CancelarButton.Location = new Point(689, 501);
+            CancelarButton.Margin = new Padding(3, 4, 3, 4);
             CancelarButton.Name = "CancelarButton";
-            CancelarButton.Size = new Size(75, 28);
+            CancelarButton.Size = new Size(86, 37);
             CancelarButton.TabIndex = 3;
             CancelarButton.Text = "Cancelar";
             CancelarButton.UseVisualStyleBackColor = true;
             // 
             // ConsultarEstadoForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(595, 431);
+            ClientSize = new Size(822, 561);
             Controls.Add(CancelarButton);
             Controls.Add(ResultadoGroupBox);
             Controls.Add(groupBox1);
             Controls.Add(label1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "ConsultarEstadoForm";
             Text = "Consulta de Encomiendas";
             groupBox1.ResumeLayout(false);
